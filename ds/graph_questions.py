945f9ba5179c918c57f42f46b2503f19774e3d6d
Harveer Singh <harveersingh93@outlook.com>
2022-02-20 15:24:59 +0530

    Added solutions for min island and island count

--- ds/graph_questions.py
+++ ds/graph_questions.py
@@ -1,5 +1,6 @@
 from collections import deque
 from ds.graph import build
+import math
 
 '''
 Check if there is path between given source and destination.
@@ -57,5 +58,153 @@ def hasUndirectedpath(graph, source, dest, visited):
             return True
     
     return False
+
+def connectedComponentCount(graph):
+    visited = set()
+    count = 0
+    for node in graph:
+        if explore(graph, node, visited):
+            count += 1
+    
+    return count
+
+
+def explore(graph, current, visited):
+    if current in visited:
+        return False
+    
+    visited.add(current)
+    
+    for neighbor in graph[current]:
+       explore(graph, neighbor, visited)
+    
+    return True
+
+"""
+count largest componet in given graph
+if edges = e and nodes = n
+we will be exploring all edge
+so,
+TC = O(e)
+SC = O(n) # we might be storing all node in set or call stack
+"""
+def largestComponent(graph):
+    maxSize = 0
+    visited = set()
+    for node in graph:
+        currentSize = exploreSize(graph, node, visited)
+        maxSize = max(maxSize, currentSize)
+    
+    return maxSize
+
+
+def exploreSize(graph, current, visited):
+    if current in visited:
+        return 0
+    visited.add(current)
+
+    count = 1
+
+    for neighbor in graph[current]:
+        count += exploreSize(graph, neighbor, visited)
+    
+    return count
+
+
+def shortestPath(edges, src, dst):
+    graph = build(edges)
+    visited = set()
+    queue = deque([(src, 0)])
+
+    while queue:
+        current, distance = queue.popleft()
+
+        if current == dst:
+            return distance
+
+        for neighbor in graph[current]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append((neighbor, distance + 1))
+    
+    return -1
+
+
+def islandCount(grid):
+    visited = set()
+    count = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if exploreIsland(grid, r, c, visited):
+                count += 1
+    
+    return count
+
+
+def exploreIsland(grid, r, c, visited):
+    rowInbound = 0 <= r and r < len(grid)
+    colInbound = 0 <= c and c < len(grid[0])
+
+    if not rowInbound or not colInbound:
+        return False
+    
+    # check if current position is water
+    if grid[r][c]=="W":
+        return False
+    
+    # check if current position has been visited
+    key = (r,c)
+    if key in visited:
+        return False
+    visited.add(key)
+
+    exploreIsland(grid, r-1, c, visited)
+    exploreIsland(grid, r+1, c, visited)
+    exploreIsland(grid, r, c-1, visited)
+    exploreIsland(grid, r, c+1, visited)
+
+    return True
+
+def minIsLand(grid):
+    visited = set()
+    minCount = math.inf
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            currentCount = exploreMinIsland(grid, r, c, visited)
+            if currentCount > 0:
+                minCount = min(minCount, currentCount)
+    
+    return minCount
+
+
+def exploreMinIsland(grid, r, c, visited):
+    rowInbount = 0 <= r and r < len(grid)
+    colInbound = 0 <= c and c < len(grid[0])
+
+    if not rowInbount or not colInbound:
+        return 0
+    
+    # now check current position if it is water.
+    if grid[r][c] =="W":
+        return 0
+    
+    key = (r,c)
+    if key in visited:
+        return 0
+    visited.add(key)
+
+    size = 1
+    size += exploreMinIsland(grid, r-1, c, visited)
+    size += exploreMinIsland(grid, r+1, c, visited)
+    size += exploreMinIsland(grid, r, c-1, visited)
+    size += exploreMinIsland(grid, r, c+1, visited)
+
+    return size
+    
+
+
+        
+
 
 
--- main.py
+++ main.py
@@ -12,31 +12,84 @@ if __name__ =="__main__":
     # graph.depthFirstTraversalInterative(g, "a")
     # print("------------------------------")
     # graph.depthFirstTraversalRecursive(g, "a")
-    g2 = {
-        "f":["g", "i"],
-        "g":["h"],
-        "h":[],
-        "i":["g", "k"],
-        "j": ["i"],
-        "k":[]
-    }
+    # g2 = {
+    #     "f":["g", "i"],
+    #     "g":["h"],
+    #     "h":[],
+    #     "i":["g", "k"],
+    #     "j": ["i"],
+    #     "k":[]
+    # }
     # has path implementations
     # print(graph_questions.hasPath(g2, "f", "g"))
     # print(graph_questions.hasPathBFS(g2, "k", "g"))
     # build graph
 
-    edges = [
-        ["i", "j"],
-        ["k", "i"],
-        ["m", "k"],
-        ["k", "l"],
-        ["o", "n"],
-    ]
+    # edges = [
+    #     ["i", "j"],
+    #     ["k", "i"],
+    #     ["m", "k"],
+    #     ["k", "l"],
+    #     ["o", "n"],
+    # ]
 
     # result = graph.build(edges)
     # print(result)
-    result = graph_questions.UndirectedPath(edges, "i", "k") #True
+    # result = graph_questions.UndirectedPath(edges, "i", "k") #True
+    # print(result)
+
+    # gg = {
+    #     3:[],
+    #     4:[6],
+    #     6:[4,5,7,8],
+    #     8:[6],
+    #     7:[6],
+    #     5:[6],
+    #     1:[2],
+    #     2:[1]
+    # }
+
+    # result = graph_questions.connectedComponentCount(gg)
+    # print(result)
+
+    # gg1 = {
+    #     0:[8,1,5],
+    #     1:[0],
+    #     5:[0,8],
+    #     8:[0,5],
+    #     2:[3,4],
+    #     3:[2,4],
+    #     4:[3,2]
+    # }
+
+    # result = graph_questions.largestComponent(gg1)
+    # print(result)
+
+
+    # edges = [
+    #     ["w", "x"],
+    #     ["x", "y"],
+    #     ["z", "y"],
+    #     ["z", "v"],
+    #     ["w", "v"],
+    # ]
+    # result = graph_questions.shortestPath(edges, "w", "z")
+    # print(result)
+
+    grid = [
+        ["W", "L", "W", "W", "W"],
+        ["W", "L", "W", "W", "W"],
+        ["W", "W", "W", "L", "W"],
+        ["W", "L", "L", "W", "W"],
+        ["L", "W", "W", "L", "L"],
+        ["L", "L", "W", "W", "W"],
+    ]
+
+    result = graph_questions.islandCount(grid)
     print(result)
+    minSize = graph_questions.minIsLand(grid)
+    print(minSize)
+
 
 
 
